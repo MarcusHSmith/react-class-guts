bf24c0d3f310ba634e5f418b97cad025dac1bd05
marcushsmith <marcushsmith@comcast.net>
2019-11-10 17:53:39 -0500

    className is lowercase

--- createReactClass.js
+++ createReactClass.js
@@ -1,18 +1,18 @@
 const fs = require("fs");
 
 const args = process.argv.slice(2);
-const className = args[0];
-console.log("CLASSNAME  ", className);
+const componentName = args[0];
+console.log("CLASSNAME  ", componentName);
 
-const propsName = `I${className}Props`;
-const stateName = `I${className}State`;
+const propsName = `I${componentName}Props`;
+const stateName = `I${componentName}State`;
 
 let createdClass = `import React from "react"
 
 export interface ${propsName} {}
 export interface ${stateName} {}
 
-class ${className} extends React.Component<${propsName}, ${stateName}> {
+class ${componentName} extends React.Component<${propsName}, ${stateName}> {
 	constructor(props) {
 		super(props)
 	}
@@ -35,12 +35,22 @@ getSnapshotBeforeUpdate(prevProps: ${propsName}, prevState: ${stateName}) {}
 componentDidUpdate(prevProps: ${propsName}, prevState: ${stateName}, snapshot) {}
 
 }
-export default ${className}
+export default ${componentName}
 
 	`;
 
+let className = "";
+for (i in componentName) {
+  const char = componentName.charAt(i);
+  if (char !== char.toUpperCase() || i == 0) {
+    className += char.toLowerCase();
+  } else {
+    className += "-" + char.toLowerCase();
+  }
+}
+
 // write to a new file named 2pac.txt
-fs.writeFile("createdClass.tsx", createdClass, err => {
+fs.writeFile(`${className}.tsx`, createdClass, err => {
   // throws an error, you could also catch it here
   if (err) throw err;
 
